68309c3daea25575a18327da6c3b883ee5285da1
Discoonect <67423146+Discoonect@users.noreply.github.com>
2020-08-04 12:01:37 +0900

    a

--- mysql_connection.js
+++ mysql_connection.js
@@ -1,11 +1,10 @@
 const mysql = require("mysql2");
-const db_config = require("./config/db_config.json");
 
 const pool = mysql.createPool({
-  host: db_config.MYSQL_HOST,
-  user: db_config.MYSQL_USER,
-  database: db_config.MYSQL_DATABASE,
-  password: db_config.MYSQL_PASSWORD,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  database: process.env.MYSQL_DATABASE,
+  password: process.env.MYSQL_PASSWORD,
   waitForConnections: true,
   connectionLimit: 10,
 });
